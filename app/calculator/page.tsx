3129ac5b34dd42ba38f4490d30926df68bd07848
‘lukasHerajt’ <lherajt@gmail.com>
2023-05-27 15:38:46 -0400

    chroma db & langchain hooked up

---
+++ app/calculator/page.tsx
@@ -0,0 +1,33 @@
+import { OpenAI } from 'langchain/llms/openai'
+import { initializeAgentExecutorWithOptions } from 'langchain/agents'
+import { SerpAPI } from 'langchain/tools'
+import { Calculator } from 'langchain/tools/calculator'
+
+export default async function page() {
+  const model = new OpenAI({ temperature: 0 })
+  const tools = [
+    new SerpAPI(process.env.SERPAPI_API_KEY, {
+      location: 'Austin,Texas,United States',
+      hl: 'en',
+      gl: 'us',
+    }),
+    new Calculator(),
+  ]
+
+  const executor = await initializeAgentExecutorWithOptions(tools, model, {
+    agentType: 'zero-shot-react-description',
+  })
+  console.log('Loaded agent.')
+
+  const input =
+    "Who is Olivia Wilde's boyfriend?" +
+    ' What is his current age raised to the 0.23 power?'
+  console.log(`Executing with input "${input}"...`)
+
+  const result = await executor.call({ input })
+
+  console.log(`Got output ${result.output}`)
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,116 @@
 import Image from 'next/image'
+import { OpenAI } from 'langchain/llms/openai'
+import { PromptTemplate } from 'langchain/prompts'
+import { LLMChain } from 'langchain/chains'
+import { initializeAgentExecutorWithOptions } from 'langchain/agents'
+import { SerpAPI } from 'langchain/tools'
+import { Calculator } from 'langchain/tools/calculator'
+import { BufferMemory } from 'langchain/memory'
+import { ConversationChain } from 'langchain/chains'
 
-export default function Home() {
+import { ChromaClient, OpenAIEmbeddingFunction } from 'chromadb'
+
+const client = new ChromaClient()
+
+const embedder = new OpenAIEmbeddingFunction({
+  openai_api_key: process?.env?.OPENAI_API_KEY!,
+})
+
+export default async function Home() {
+  const collection = await client.createCollection({
+    name: 'my_collection',
+    embeddingFunction: embedder,
+  })
+
+  await collection.add({
+    ids: ['id1', 'id2'],
+    metadatas: [{ source: 'my_source' }, { source: 'my_source' }],
+    documents: ['This is a document', 'This is another document'],
+  })
+
+  const results = await collection.query({
+    n_results: 2,
+    query_text: ['This is a query document'],
+  })
+
+  console.log('results: ', results)
+
+  // Streaming
+  const chat = new OpenAI({
+    streaming: true,
+    callbacks: [
+      {
+        handleLLMNewToken(token: string) {
+          process.stdout.write(token)
+        },
+      },
+    ],
+  })
+
+  await chat.call('Write me an eminem song about vector databases.')
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      main page
     </main>
   )
 }
+
+// basic example
+// const model = new OpenAI({ temperature: 0.9 })
+// const template = 'What is a good name for a company that makes {product}?'
+// const prompt = new PromptTemplate({
+//   template: template,
+//   inputVariables: ['product'],
+// })
+// const chain = new LLMChain({ llm: model, prompt: prompt })
+// const res = await chain.call({ product: 'ice cream' })
+// console.log(res)
+
+// const res2 = await prompt.format({ product: 'popsicles' })
+// console.log(res2)
+
+// const res3 = await model.call(res2)
+// console.log(res3)
+
+// Run based on user input
+// const model = new OpenAI({ temperature: 0 })
+// const tools = [
+//   new SerpAPI(process.env.SERPAPI_API_KEY, {
+//     location: 'Austin,Texas,United States',
+//     hl: 'en',
+//     gl: 'us',
+//   }),
+//   new Calculator(),
+// ]
+
+// const executor = await initializeAgentExecutorWithOptions(tools, model, {
+//   agentType: 'zero-shot-react-description',
+// })
+// console.log('Loaded agent.')
+
+// const input =
+//   "Who is Olivia Wilde's boyfriend?" +
+//   ' What is his current age raised to the 0.23 power?'
+// console.log(`Executing with input "${input}"...`)
+
+// const result = await executor.call({ input })
+
+// console.log(`Got output ${result.output}`)
+
+// // Chains and agents
+// const model = new OpenAI({})
+// const memory = new BufferMemory()
+// const chain = new ConversationChain({ llm: model, memory: memory })
+// const res1 = await chain.call({ input: "Hi! I'm Jim." })
+// console.log(res1)
+
+// {
+//   response: " Hi Jim! It's nice to meet you. My name is AI. What would you like to talk about?"
+// }
+
+// const res2 = await chain.call({ input: "What's my name?" })
+// console.log(res2)
+
+// {
+//   response: ' You said your name is Jim. Is there anything else you would like to talk about?'
+// }
